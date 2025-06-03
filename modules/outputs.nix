16473cd1dc521ae660886449dbaf8bd69f737346
iemptybrass <187961073+iemptybrass@users.noreply.github.com>
2025-06-02 21:28:24 -0400

    Update outputs.nix

--- modules/outputs.nix
+++ modules/outputs.nix
@@ -4,17 +4,15 @@ with lib;
 
 {
   options.outputs = mkOption {
-    type = types.submodule {
+    type = lib.types.submodule {
       options = {
         modules = mkOption {
-          type = types.listOf types.unspecified;
+          type = lib.types.listOf lib.types.unspecified;
           default = [];
-          description = "Fake outputs.modules list.";
         };
       };
     };
     default = {};
-    description = "Fake Flake-like outputs with modules.";
   };
 
   config = {};
