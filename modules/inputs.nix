08179deb537aa9aba22c13273fcfdc3ae3ff3b22
iemptybrass <187961073+iemptybrass@users.noreply.github.com>
2025-06-02 21:08:10 -0400

    Update inputs.nix

--- modules/inputs.nix
+++ modules/inputs.nix
@@ -10,22 +10,18 @@ with lib;
           url = lib.mkOption {
             type = lib.types.str;
             default = "";
-            description = "Fake flake input URL.";
           };
           type = lib.mkOption {
             type = lib.types.str;
             default = "";
-            description = "Source type (e.g., github, git, etc).";
           };
           owner = lib.mkOption {
             type = lib.types.str;
             default = "";
-            description = "Repository owner.";
           };
           repo = lib.mkOption {
             type = lib.types.str;
             default = "";
-            description = "Repository name.";
           };
           inputs = lib.mkOption {
             type = lib.types.attrsOf (
@@ -34,19 +30,16 @@ with lib;
                   follows = lib.mkOption {
                     type = lib.types.str;
                     default = "";
-                    description = "Fake follows for nested inputs.";
                   };
                 };
               }
             );
             default = {};
-            description = "Nested fake flake inputs.";
           };
         };
       }
     );
     default = {};
-    description = "Fake Flake-like inputs with url, type, owner, repo, and nested follows.";
   };
 
   config = {};
