19c78ff3ac6fffa13a7299ee9860bde11d14cbe5
iemptybrass <187961073+iemptybrass@users.noreply.github.com>
2025-06-03 20:52:09 -0400

    Update inputs.nix

--- modules/inputs.nix
+++ modules/inputs.nix
@@ -10,18 +10,25 @@ with lib;
           url = lib.mkOption {
             type = lib.types.str;
             default = "";
+            description = "URL of the flake input.";
           };
           type = lib.mkOption {
             type = lib.types.str;
             default = "";
+            description = "Type of the flake input (e.g., github).";
           };
           owner = lib.mkOption {
             type = lib.types.str;
             default = "";
+            description = "Owner of the repository.";
           };
           repo = lib.mkOption {
             type = lib.types.str;
             default = "";
+            description = "Repository name.";
+          };
+          nixosModules = lib.mkOption {
+            type = lib.types.unspecified;
           };
           inputs = lib.mkOption {
             type = lib.types.attrsOf (
