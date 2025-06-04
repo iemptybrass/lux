61fe3110616e644fbb43fdcbe29588fa90e612ab
iemptybrass <187961073+iemptybrass@users.noreply.github.com>
2025-06-03 21:14:20 -0400

    Update inputs.nix

--- modules/inputs.nix
+++ modules/inputs.nix
@@ -1,30 +1,19 @@
-{ lib, config, ... }:
+{ lib, ... }:
 
 with lib;
 
 {
   options.inputs = lib.mkOption {
     type = lib.types.attrsOf (
-      lib.types.submodule ({ config, ... }: {
+      lib.types.submodule {
         options = {
           url = lib.mkOption {
             type = lib.types.str;
             default = "";
           };
-          type = lib.mkOption {
-            type = lib.types.str;
-            default = "";
-          };
-          owner = lib.mkOption {
-            type = lib.types.str;
-            default = "";
-          };
-          repo = lib.mkOption {
-            type = lib.types.str;
-            default = "";
-          };
           nixosModules = lib.mkOption {
-            type = lib.types.unspecified;
+            type = lib.types.str;
+            default = "";
           };
           inputs = lib.mkOption {
             type = lib.types.attrsOf (
@@ -40,37 +29,10 @@ with lib;
             default = {};
           };
         };
-
-        config = lib.mkMerge [
-          (lib.mkIf true (
-            lib.mkAssert (
-              let
-                urlSet = config.url != "";
-                typeSet = config.type != "";
-                ownerSet = config.owner != "";
-                repoSet = config.repo != "";
-
-                anySet = typeSet || ownerSet || repoSet;
-                allSet = typeSet && ownerSet && repoSet;
-
-                invalid =
-                  if anySet then
-                    urlSet || (!allSet)
-                  else
-                    !urlSet;
-              in
-                !invalid
-            ) ''
-              Invalid flake input setup:
-              - If `type`, `owner`, or `repo` are set, `url` must be unset and all of `type`, `owner`, `repo` must be set.
-              - Otherwise, `url` must be set and `type`, `owner`, `repo` must be unset.
-            ''
-          ))
-        ];
-      })
+      }
     );
     default = {};
   };
 
-  config._forceInputsEval = lib.mkForce config.inputs;
+  config = {};
 }
